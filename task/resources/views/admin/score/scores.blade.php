20516886677132ee54518fbd5c4235ac89e04718
Senthilkumar-2003-java <senthilson2003@gmail.com>
2025-04-09 21:54:32 +0530

    commit responsive score borard table

--- task/resources/views/admin/score/scores.blade.php
+++ task/resources/views/admin/score/scores.blade.php
@@ -22,13 +22,15 @@
 
 <body class="bg-gray-900 text-white">
 
-  <div class="max-w-full w-full px-4 sm:px-6 md:px-8 lg:px-10 py-6">
-    <h3 class="text-lg font-semibold text-white mb-4">My Score Board</h3>
+<div class=" max-w-full w-full ml-[10px] mr-[16px] py-6">
 
-    <div class="overflow-x-auto">
-      <table class="w-full  border-collapse border border-gray-600 text-sm sm:text-base text-white">
+
+  <h3 class="text-lg font-semibold text-black mb-4">My Score Board</h3>
+
+  <div class="w-full overflow-x-auto">
+    <table class="min-w-[800px] w-full border-collapse border border-gray-600 text-sm sm:text-base text-white ">
         <thead>
-          <tr class="bg-purple-500 text-white text-left">
+          <tr class="bg-purple-500 text-white text-center">
             <th class="border border-gray-600 px-3 py-2 whitespace-nowrap">ID</th>
             <th class="border border-gray-600 px-3 py-2 whitespace-nowrap">Task Title</th>
             <th class="border border-gray-600 px-3 py-2 whitespace-nowrap">Status</th>
@@ -39,7 +41,7 @@
         </thead>
         <tbody>
           @foreach ($tasks as $task)
-          <tr class="bg-gray-900 hover:bg-gray-700 transition">
+          <tr class="bg-gray-900 hover:bg-gray-700 transition text-center">
             <td class="border border-gray-600 px-3 py-2">{{ $task->id }}</td>
             <td class="border border-gray-600 px-3 py-2">{{ $task->task_title }}</td>
             <td class="border border-gray-600 px-3 py-2">{{ $task->status }}</td>
